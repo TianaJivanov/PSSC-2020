1e1843f376c039176085b4e6418910697e946867
Tiana <tiana_jivanov@yahoo.com>
2020-10-21 01:53:26 +0300

    L04

---
+++ Jivanov-Tiana/L04/L04/Test.App/ProgramAnswer.cs
@@ -0,0 +1,66 @@
+﻿using Profile.Domain.PostAnswerWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net;
+using CSharp.Choices;
+using static Profile.Domain.PostAnswerWorkflow.PostAnswerResult;
+
+namespace Test.App
+{
+    class ProgramAnswer
+    {
+
+
+        // Este pus in comentariu deoarece nu poti avea 2 Main. Am deja unul in ProgramQuestion.
+
+        //static void Main(string[] args)
+        //{
+        //    var cmd = new PostAnswerCmd("Once I query for the List<T>, how do I add my initial Item, not part of the data source, as the FIRST element in that List<T> ");
+        //    var result = PostAnswer(cmd);
+        //    result.Match(
+        //        ProcessAnswerPosted,
+        //        ProcessAnswerNotPosted,
+        //        ProcessInvalidAnswer);
+        //    Console.ReadLine();
+        //}
+
+
+
+        private static IPostAnswerResult ProcessInvalidAnswer(AnswerValidationFailed validationErrors)
+        {
+            Console.WriteLine("Answer validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+
+
+        private static IPostAnswerResult ProcessAnswerNotPosted(AnswerNotPosted answerNotPostedResult)
+        {
+            Console.WriteLine($"Answer not posted: {answerNotPostedResult.Reason}");
+            return answerNotPostedResult;
+        }
+
+        private static IPostAnswerResult ProcessAnswerPosted(PostAnswer answer)
+        {
+            Console.WriteLine($"Answer: {answer.AnswerText}");
+            return answer;
+        }
+
+        public static IPostAnswerResult PostAnswer(PostAnswerCmd postAnswerCommand)
+        {
+            if (string.IsNullOrEmpty(postAnswerCommand.AnswerText))
+            {
+                var errors = new List<string>() { "Invalid answer body" };
+                return new AnswerValidationFailed(errors);
+            }
+
+            var result = new PostAnswer(postAnswerCommand.AnswerText);
+            return result;
+        }
+    }
+}
---
+++ Jivanov-Tiana/L04/L04/Test.App/ProgramQuestion.cs
@@ -0,0 +1,80 @@
+﻿ using Profile.Domain.CreateQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using static Profile.Domain.CreateQuestionWorkflow.CreateQuestionResult;
+
+namespace Test.App
+{
+    class ProgramQuestion
+    {
+        static void Main(string[] args)
+        {
+            var tag = "c#".Split(',').ToList();
+            var cmd = new CreateQuestionCmd("Does dontDestroyOnLoad only work a single time ? ", "Can someone give me concrete details ? ", tag);
+            var result = CreateQuestion(cmd);
+
+            result.Match(
+                    ProcessQuestionPosted,
+                    ProcessQuestionNotPosted,
+                    ProcessInvalidQuestion
+                );
+
+            Console.ReadLine();
+        }
+
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+           
+
+        private static ICreateQuestionResult ProcessQuestionNotPosted(QuestionNotPosted questionNotPostedResult)
+        {
+            Console.WriteLine($"Question not posted: {questionNotPostedResult.Reason}");
+            return questionNotPostedResult;
+        }
+
+        private static ICreateQuestionResult ProcessQuestionPosted(QuestionPosted question)
+        {
+            Console.WriteLine($"Question: {question.Title}");
+            return question;
+        }
+
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if (string.IsNullOrEmpty(createQuestionCommand.Title))
+            {
+                var errors = new List<string>() { "Invalid question title" };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (string.IsNullOrEmpty(createQuestionCommand.Body))
+            {
+                var errors = new List<string>() { "Invalid question body" };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (createQuestionCommand.Tags.Count==0)
+            {
+                var errors = new List<string>() { "Invalid question tags" };
+                return new QuestionValidationFailed(errors);
+            }
+
+
+            //if (new Random().Next(10) > 1)
+            //{
+            //    return new QuestionNotPosted("Title could not be verified");
+            //}
+
+            var result = new QuestionPosted(createQuestionCommand.Title);
+            return result;
+        }
+    }
+}
